55d8ce869815cbea231e027fc6ce49b7ce55b94b
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-04-17 21:08:48 +0300

    Moved type check, some visual fixes

--- Animal.py
+++ Animal.py
@@ -14,13 +14,17 @@ class Animal:
         self.parts = []
 
     def set_sex(self, sex: Sex):
+        if not isinstance(sex, Sex) and not issubclass(sex, Sex):
+            raise TypeError(f"{sex} should be class Sex")
         self.sex = sex
 
     def set_color(self, color: Color):
+        if not isinstance(color, Color) and not issubclass(color, Color):
+            raise TypeError(f"{color} should be class Color")
         self.color = color
 
     def __str__(self) -> str:
-        return f"{self.name} is {self.age} years old {self.sex} {self.color} {self.__class__.__name__.lower()}" + [f"with {', '.join(self.parts)}", ""][self.parts == []]
+        return f"{self.name} is {self.age} years old {self.sex} {self.color} {self.__class__.__name__.lower()}" + [f" with {', '.join(self.parts)}", ""][self.parts == []]
 
 
 class Human(Animal):
